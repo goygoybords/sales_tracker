71016038cf39025a154cdac1269428997d06b0fb
goygoybords <kevinseankho@yahoo.com>
2016-11-29 15:14:29 +0800

    updated bug of custoner list

--- process/ajax/customer_list.php
+++ process/ajax/customer_list.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * DataTables example server-side processing script.
  *
@@ -12,14 +13,18 @@
  *
  * @license MIT - http://datatables.net/license_mit
  */
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Easy set variables
  */
+
 // DB table to use
 session_start();
 $table = 'customer';
+
 // Table's primary key
 $primaryKey = 'id';
+
 // Array of database columns which should be read and sent back to DataTables.
 // The `db` parameter represents the column name in the database, while the `dt`
 // parameter represents the DataTables column identifier. In this case simple
@@ -54,6 +59,7 @@ $columns = array(
             },
             'field' => 'id' )
     );
+
 // SQL server connection information
 $sql_details = array(
     'user' => 'root',
@@ -61,10 +67,13 @@ $sql_details = array(
     'db'   => 'sales_tracker',
     'host' => 'localhost'
 );
+
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * If you just want to use the basic configuration for DataTables with PHP
  * server-side, there is no need to edit below this line.
  */
+
     // require( 'ssp.php' );
     require('ssp.customized.class.php' );
     
@@ -84,8 +93,10 @@ $sql_details = array(
     }
     else
     {
+        $joinQuery = "FROM customer c";
          $extraWhere =  "c.status = 1" ;
     }
     echo json_encode(
         SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns, $joinQuery, $extraWhere )
     );
+
